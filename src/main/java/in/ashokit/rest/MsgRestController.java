7be067a397ec37d3a4a9a61c773afbaac0da46aa
kpurnachandrarao99 <k.purnachandrarao1996@gmail.com>
2026-07-16 13:14:32 +0530

    modified

--- src/main/java/in/ashokit/rest/MsgRestController.java
+++ src/main/java/in/ashokit/rest/MsgRestController.java
@@ -17,8 +17,8 @@ public class MsgRestController {
 	public String getMsg() {
 		int i = 50;
 		int j = 60;
-		int age = 16;
-		int k = 30;
+		int age = 26;
+		int k = 70;
 		return msgService.getWelcomeMsg();
 	}
 	
